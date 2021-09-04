78ef59f283d966effa27e597cbb9bf059b602a79
Freeman-md <freemanmadudili@gmail.com>
2021-09-04 12:03:08 +0100

    Setup Dynamic Joke Page

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    {{ $route.params.id }}
+    <nuxt-link to="/jokes">Back To Jokes</nuxt-link>
+    <h2>{{ joke }}</h2>
+    <hr>
+    <small>Joke ID: {{$route.params.id}}</small>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data() {
+    return {
+      joke: null
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
 
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+
+      this.joke = res.data.joke
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
 </script>
 
